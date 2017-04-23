6288006cb18510cfe0aef75b4d06f3dda09eac69
IrinaIalova <irina@dyna.mo>
2017-04-23 17:49:17 +0200

    Попробовала вычисление по формуле

--- sandbox/src/main/java/ru/point/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/point/pft/sandbox/MyFirstProgram.java
@@ -4,5 +4,17 @@ public class MyFirstProgram {
 
 public static void main (String[] args){
   System.out.println("Hello, world!");
+
+
+    double xb= 2.0;
+    double xa= 1.0;
+    double yb= 2.0;
+    double ya= 1.0;
+    double ab = Math.sqrt((xb - xa)*(xb - xa) + (yb - ya)*(yb - ya));
+
+    System.out.println("Расстояние между двумя точками на плоскости " + ab );
+
+
 }
 }
+
