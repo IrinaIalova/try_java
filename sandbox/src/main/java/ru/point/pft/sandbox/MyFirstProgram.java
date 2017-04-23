a594e56c67e73900daaab9650af9eff140d1b1c5
IrinaIalova <irina@dyna.mo>
2017-04-23 18:38:22 +0200

    Задание 2, пункт 1-3

--- sandbox/src/main/java/ru/point/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/point/pft/sandbox/MyFirstProgram.java
@@ -3,18 +3,15 @@ package ru.point.pft.sandbox;
 public class MyFirstProgram {
 
 public static void main (String[] args){
-  System.out.println("Hello, world!");
 
-
-    double xb= 2.0;
-    double xa= 1.0;
-    double yb= 2.0;
-    double ya= 1.0;
-    double ab = Math.sqrt((xb - xa)*(xb - xa) + (yb - ya)*(yb - ya));
-
-    System.out.println("Расстояние между двумя точками на плоскости " + ab );
-
-
+    Point a = new Point(1.0,1.0);
+    Point b = new Point(2.0,2.0);
+    System.out.println("Расстояние между двумя точками " + distance(a, b));
 }
+
+public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+}
+
 }
 
