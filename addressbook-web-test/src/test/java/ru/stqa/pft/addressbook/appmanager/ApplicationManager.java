1e5f08b7afb87b796934eb465ba1075f3dbf3376
IrinaIalova <irina@dyna.mo>
2017-05-06 10:01:11 +0200

    I3_7(1). Add browsers. Only Firefox works

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,21 +1,39 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.safari.SafariDriver;
+
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by iriska on 5/1/17.
  */
 public class ApplicationManager {
-    FirefoxDriver wd;
+    WebDriver wd;
 
     private ContactHelper contactHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
+        if (browser == BrowserType.FIREFOX){
         wd = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME){
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.SAFARI) {
+            wd = new SafariDriver();
+
+            }
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/index.php");
         groupHelper = new GroupHelper(wd);
